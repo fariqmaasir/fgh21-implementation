47de4d794dcd3468f868f87c27a11573f4149a2d
Fariq <144993765+fariqmaasir@users.noreply.github.com>
2024-06-10 17:02:40 +0700

    Add files via upload

---
+++ task4.js
@@ -0,0 +1,7 @@
+fetch("https://jsonplaceholder.typicode.com/users")
+.then((res) => {
+  res.json().then(data => {
+    const names = data.map(user => user.name);
+    console.log(names);
+  });
+});
